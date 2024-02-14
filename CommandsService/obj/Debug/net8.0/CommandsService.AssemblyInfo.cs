d46bc7f414f2ffdd7dc52d997821baaa86d51908
Prabuddhika96 <prabuddhika1996@gmail.com>
2024-02-14 10:16:08 +0530

    create docker file

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad47cf8ef6769861de2752dc40e73097fdc1a29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b2fdf39e8e38c8d63e5d5b7cb336f1fc89fbe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
